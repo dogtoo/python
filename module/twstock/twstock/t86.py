ad714b8753c844fe1817c09856c7d90bccf28212
dogtoo <dogtoo.lin@gmail.com>
2019-06-22 01:59:07 +0800

    Update t86.py

--- module/twstock/twstock/t86.py
+++ module/twstock/twstock/t86.py
@@ -1,3 +1,6 @@
+import datetime
+import json
+import time
 import requests
 import twstock
 #http://www.twse.com.tw/fund/T86?response=json&date=20190528&selectType=01&_=1559145215220
@@ -5,7 +8,8 @@ STOCKINFO_URL = 'http://www.twse.com.tw/fund/T86'
 
 req = requests.Session()
 req.get(STOCKINFO_URL)
-p = {'response': 'json', 'date': '20190501', 'selectType':'01', '_':int(time.time()) * 1000))}
+time=int(time.time()) * 1000))
+p = {'response': 'json', 'date': '20190501', 'selectType':'01', '_':time}
 r = req.get(STOCKINFO_URL, params=p)
 print(r.url)
 print(r.json)
