df5538e0616a23b4bc33dcf8e19e0c0f7059dc61
dogtoo <dogtoo.lin@gmail.com>
2019-06-22 01:46:24 +0800

    t86

--- module/twstock/twstock/t86.py
+++ module/twstock/twstock/t86.py
@@ -1,1 +1,11 @@
-http://www.twse.com.tw/fund/T86?response=json&date=20190528&selectType=01&_=1559145215220
+import requests
+import twstock
+#http://www.twse.com.tw/fund/T86?response=json&date=20190528&selectType=01&_=1559145215220
+STOCKINFO_URL = 'http://www.twse.com.tw/fund/T86'
+
+req = requests.Session()
+req.get(STOCKINFO_URL)
+p = {'response': 'json', 'date': '20190501', 'selectType':'01', '_':int(time.time()) * 1000))}
+r = req.get(STOCKINFO_URL, params=p)
+print(r.url)
+print(r.json)
