9ae8191d50ff672a2de04ecc284f25d7d7ea0e2a
dogtoo <dogtoo.lin@gmail.com>
2019-11-16 20:26:43 +0800

    Update realTimeBak.py

--- stock/app/realTimeBak.py
+++ stock/app/realTimeBak.py
@@ -21,7 +21,7 @@ collRT = db[collname]
 collRTBak = db[bkcollname]
 
 def daterange(start_date, end_date):
-    if int ((end_date - start_date).days) == 0
+    if int ((end_date - start_date).days) == 0:
         yield start_date
     for n in range(int ((end_date - start_date).days)):
         yield start_date + timedelta(n)
