0ec7d2337a0e399239b2d76dc1760bccba44e9db
dogtoo <dogtoo.lin@gmail.com>
2019-11-17 00:15:15 +0800

    Update realTimeBak.py

--- stock/app/realTimeBak.py
+++ stock/app/realTimeBak.py
@@ -9,6 +9,25 @@ import re
 bakComm = str(sys.argv[1])
 startDate = str(sys.argv[2])
 endDate = str(sys.argv[3])
+mode = bool(sys.argv[4])
+
+if len(sys.argv) == 0:
+    print("need give argv[1] bakComm{'bak','res'} \n")
+    print("          argv[2] startDate{'YYYYMMDD'} \n")
+    print("          argv[3] endDate{'YYYYMMDD'} \n")
+    print("          argv[4] mode{1:test} not need \n")
+    print("  or give two argv for filename")
+
+if len(bakComm) == 0:
+    sys.exit(0)
+
+if len(sys.argv) == 2:
+    filename = str(sys.argv[2])
+else:
+    if len(startDate) == 0:
+        sys.exit(0)
+    if len(endDate) == 0:
+        sys.exit(0)
 
 host = "172.18.0.2"
 port = "27017"
@@ -41,44 +60,62 @@ def run_backup(date_):
             f.write(json.dumps(d) + "\n")
             
     f.close()
+
+def restore_file(file):
+    with open(file, 'r') as jsonData:
+        try:
+            line = jsonData.readline();
+            while line:
+                jsObj = json.loads(line)
+                value = { "$set": jsObj }
+                if "final_trade_volume" not in jsObj:
+                    query = {"code":jsObj.get('code'),"date":jsObj.get('date'),"accumulate_trade_volume":{"$gte":jsObj.get('accumulate_trade_volume')}}
+                else:
+                    query = {"code":jsObj.get('code'),"date":jsObj.get('date'),"final_trade_volume":jsObj.get('final_trade_volume')}
+                
+                if mode:
+                    collRTBak.update_one(query, value, upsert=True)
+                else:
+                    collRT.update_one(query, value, upsert=True)
+                #print(jsObj)
+                #print(type(jsObj)) 
+                #for key in jsObj.keys(): 
+                #    print('key: %s  value: %s' % (key,jsObj.get(key))) 
+                line = jsonData.readline();
+        except BaseException:
+            print(f + " load error ")
+        else:
+            jsonData.close()
+            print(f + " load success")
     
 def run_restore(date_):
-    for f in os.listdir(outputs_dir):
-        if re.search(date_, f):
-            print(f)
-            if int(os.stat(outputs_dir + f).st_size) > 0:
-                with open(outputs_dir + f, 'r') as jsonData:
-                    try:
-                        line = jsonData.readline();
-                        while line:
-                            jsObj = json.loads(line)
-                            query = {"code":jsObj.get('code'),"date":jsObj.get('date'),"accumulate_trade_volume":{"$gte":jsObj.get('accumulate_trade_volume')}}
-                            value = { "$set": jsObj }
-                            if "final_trade_volume" not in jsObj:
-                                collRTBak.update_one(query, value, upsert=True)
-                            else:
-                                query = {"code":jsObj.get('code'),"date":jsObj.get('date'),"final_trade_volume":jsObj.get('final_trade_volume')}
-                                collRTBak.update_one(query, value, upsert=True)
-                            #print(jsObj)
-                            #print(type(jsObj)) 
-                            #for key in jsObj.keys(): 
-                            #    print('key: %s  value: %s' % (key,jsObj.get(key))) 
-                            line = jsonData.readline();
-                    except BaseException:
-                        print(f + " load error ")
-                    else:
-                        jsonData.close()
-                        print(f + " load success")
-    
-start_date = date(int(startDate[0:4]), int(startDate[5:6]), int(startDate[6:8]))
-end_date = date(int(endDate[0:4]), int(endDate[5:6]), int(endDate[6:8]))
-for date_ in daterange(start_date, end_date):
-    fdate = date_.strftime("%Y%m%d")
-    print(fdate)
+    if len(filename) > 0
+        restore_file(outputs_dir + filename)
+    else:
+        for f in os.listdir(outputs_dir):
+            if re.search(date_, f):
+                print(f)
+                if int(os.stat(outputs_dir + f).st_size) > 0:
+                    restore_file(outputs_dir + f)
+
+if len(filename) > 0 :
+    print(filename)
     if bakComm == 'bak':
-        run_backup(fdate)
+        run_backup(filename)
     elif bakComm == 'res':
-        run_restore(fdate)
+        run_restore(filename)
     else:
         print ('function error argv[1] need "bak" or "res"')
+else:  
+    start_date = date(int(startDate[0:4]), int(startDate[5:6]), int(startDate[6:8]))
+    end_date = date(int(endDate[0:4]), int(endDate[5:6]), int(endDate[6:8]))
+    for date_ in daterange(start_date, end_date):
+        fdate = date_.strftime("%Y%m%d")
+        print(fdate)
+        if bakComm == 'bak':
+            run_backup(fdate)
+        elif bakComm == 'res':
+            run_restore(fdate)
+        else:
+            print ('function error argv[1] need "bak" or "res"')
     
