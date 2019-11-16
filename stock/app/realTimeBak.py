644c96c5f332ef2500fd71350889486838bdb787
dogtoo <dogtoo.lin@gmail.com>
2019-11-17 00:19:06 +0800

    Update realTimeBak.py

--- stock/app/realTimeBak.py
+++ stock/app/realTimeBak.py
@@ -10,7 +10,7 @@ bakComm = str(sys.argv[1])
 startDate = str(sys.argv[2])
 endDate = str(sys.argv[3])
 print(len(sys.argv))
-if len(sys.argv) == 4:
+if len(sys.argv) == 5:
     mode = bool(sys.argv[4])
 
 if len(sys.argv) == 0:
