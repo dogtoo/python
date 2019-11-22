7f7c736141efa52cd884476d1ef8ea1eb2a6592c
dogtoo <dogtoo.lin@gmail.com>
2019-11-22 18:18:09 +0800

    up doc data

---
+++ stock/mongoSql/aggregate_TWSE_addColumn.js
@@ -0,0 +1,47 @@
+db.getCollection('t86').aggregate([
+    {
+        $match:{
+            $and:[
+                {date:{'$gte':'20191101', '$lte':'20191122'}}
+            ]
+            
+        }
+    },
+    {
+        $lookup:{
+            from:'TWSE',
+            localField:'code',
+            foreignField:'code',
+            as:'codeinfo'}
+    },
+    {
+        $unwind:'$codeinfo'
+    },
+    {
+        $project:{
+            'code':1,'DHedge_I':1,'groupCode':'$codeinfo.groupCode'
+        }
+    }
+    /*,
+    {
+        $redact: {
+            $cond: [
+                {'$eq':['$codeinfo',[]]}
+               ,"$$PRUNE","$$DESCEND"
+            ]
+        }
+    },
+    {
+        $project:{
+            'code':1,'DHedge_I':1,
+            'codeinfo':{
+                $filter:{
+                    input:'$codeinfo',
+                    as:'groupCode',
+                    cond:{'$eq':['$$groupCode.groupCode','01']}
+                }
+            }
+        }
+    }
+    */
+])
