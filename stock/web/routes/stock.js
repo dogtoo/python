711dc498aac1bb0fd3e56edca15adbfc7be2598d
dogtoo <dogtoo.lin@gmail.com>
2019-12-12 18:09:38 +0800

    web draw painter stream add

--- stock/web/routes/stock.js
+++ stock/web/routes/stock.js
@@ -1,12 +1,24 @@
 const router = require('koa-router')();
 
 // Router -> /
-router.get('/getT86', async(ctx) => {
+router.get('/kline', async(ctx) => {
     console.log(`   getT86`);
     let code = ctx.request.query.code;
-    await ctx.render('index', {
+    await ctx.render('kline', {
         msg:'stock:' + code
     })
+});
+
+router.get('/absPainter.js', async(ctx) => {
+    await ctx.render('js/absPainter')
+});
+
+router.get('/axis-x.js', async(ctx) => {
+    await ctx.render('js/axis-x')
+});
+
+router.get('/axis-y.js', async(ctx) => {
+    await ctx.render('js/axis-y')
 });
 
 module.exports = router;
