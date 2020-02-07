fa1d8eb940c71b5a6e3eae72fde942553ea67699
dogtoo <dogtoo.lin@gmail.com>
2020-02-07 18:03:15 +0800

    調整range的筆數
    range 有算到頭尾，所以getDataLength時要多一筆
    取線軸時不算尾所以要扣1

---
+++ stock/mongoSql/realtime.js
@@ -0,0 +1,24 @@
+db.getCollection('realtime').aggregate([
+    {
+        $match:{
+            $and:[
+                {code:{'$eq':'2002'}
+               , date:{'$eq':'20200206'}
+                }
+            ]            
+        }
+    }, {
+        $project:{
+            '_id':0,
+            'code':1,
+            'date':1,
+            'time':'$final_time',
+            'price':'$latest_trade_price',
+            'vol':'$trade_volume'
+       }
+    }, {
+        $sort:{
+            'time':1
+        }
+    }
+])
