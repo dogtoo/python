72b9b2de465daaa9f8f766d99a38f4b251245a32
dogtoo <dogtoo.lin@gmail.com>
2020-03-24 01:37:08 +0800

    Create sortwith00.js

---
+++ stock/mongoSql/sortwith00.js
@@ -0,0 +1,4 @@
+db.getCollection('stockDay').find({'date':'20200323',$or:[
+                    {groupCode:{'$ne':'00'}}
+                    ,{groupCode:{'$eq':'00'}, Transaction:{$gt:100}}
+                 ]}).sort({Transaction:-1}).count()
